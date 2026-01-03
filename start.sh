c7495babda8794955cb840bb4601f558bd0b515e
subszaid-boop <subszaid@gmail.com>
2026-01-03 04:07:00 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
 set -e
 
-echo "[+] Starting services..."
-
-# DBus
 mkdir -p /var/run/dbus
 dbus-daemon --system --fork
-echo "[✓] DBus started"
 
-# SSH
-service ssh start
-echo "[✓] SSH started"
+/usr/sbin/sshd
 
-# X11 socket directory
-mkdir -p /tmp/.X11-unix
-chmod 1777 /tmp/.X11-unix
+/usr/sbin/xrdp-sesman &
 
-# XRDP needs sesman
-service xrdp-sesman start
-echo "[✓] XRDP sesman started"
-
-# Start XRDP in foreground (Docker safe)
-echo "[✓] XRDP started"
 exec /usr/sbin/xrdp --nodaemon
